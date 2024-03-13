bdce9d818b89db48c9811677f6cff50999c502c4
exicc <ping.u@ya.ru>
2024-03-14 00:27:02 +0300

    Added dummy test

---
+++ app/src/test/java/TestExample.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestExample {
+    @Test
+    void test1() {
+        assertTrue(true);
+    }
+}

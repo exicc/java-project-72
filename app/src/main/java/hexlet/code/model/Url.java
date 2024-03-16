da072715f27a6c83a799f662df4242a08abd42ff
exicc <ping.u@ya.ru>
2024-03-17 01:03:56 +0300

    Replaced LocalDate to Timestamp

--- app/src/main/java/hexlet/code/model/Url.java
+++ app/src/main/java/hexlet/code/model/Url.java
@@ -2,15 +2,15 @@ package hexlet.code.model;
 
 import lombok.Getter;
 import lombok.Setter;
-import java.time.LocalDateTime;
+import java.sql.Timestamp;
 
 @Setter
 @Getter
 public class Url {
     Long id;
     String name;
-    LocalDateTime createdAt;
-    public Url(String name, LocalDateTime createdAt) {
+    Timestamp createdAt;
+    public Url(String name, Timestamp createdAt) {
         this.name = name;
         this.createdAt = createdAt;
     }

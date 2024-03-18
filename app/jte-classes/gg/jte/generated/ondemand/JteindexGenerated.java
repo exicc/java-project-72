e5e3b42de1b9bf5b9f98aa9458960a927618a9b8
exicc <ping.u@ya.ru>
2024-03-18 03:51:59 +0300

    Generated jte dir

---
+++ app/jte-classes/gg/jte/generated/ondemand/JteindexGenerated.java
@@ -0,0 +1,11 @@
+package gg.jte.generated.ondemand;
+public final class JteindexGenerated {
+	public static final String JTE_NAME = "index.jte";
+	public static final int[] JTE_LINE_INFO = {14,14,14,14,14,14,14,14,14,14,14};
+	public static void render(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor) {
+		jteOutput.writeContent("<!doctype html>\n<html lang=\"en\">\n<head>\n    <meta charset=\"utf-8\" />\n    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" />\n    <title>Hello World</title>\n</head>\n<body>\n<div class=\"col-lg-8 mx-auto p-4 py-md-5\">\n    <main>\n        <h1 class=\"text-body-emphasis\">Hello, World!</h1>\n    </main>\n</div>\n</body>\n</html>");
+	}
+	public static void renderMap(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, java.util.Map<String, Object> params) {
+		render(jteOutput, jteHtmlInterceptor);
+	}
+}

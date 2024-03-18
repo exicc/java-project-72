ce92b70c7817919dd2b93e35d8179879d1aa43dd
exicc <ping.u@ya.ru>
2024-03-18 03:50:34 +0300

    Upgraded jte version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,13 +22,13 @@ repositories {
 }
 
 dependencies {
-    implementation("com.h2database:h2:2.2.222")
+    implementation("com.h2database:h2:2.2.224")
     implementation("com.zaxxer:HikariCP:5.0.1")
     implementation("org.postgresql:postgresql:42.7.3")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
     implementation("org.apache.commons:commons-text:1.10.0")
-    implementation("gg.jte:jte:3.1.0")
-    implementation("org.slf4j:slf4j-simple:2.0.12")
+    implementation("gg.jte:jte:3.1.9")
+    //implementation("org.slf4j:slf4j-simple:2.0.12")
     implementation("io.javalin:javalin:6.1.3")
     implementation("io.javalin:javalin-bundle:6.1.3")
     implementation("io.javalin:javalin-rendering:6.1.3")

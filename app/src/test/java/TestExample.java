1dd051e8bf8a9d8d2980722e813fa7090573b711
exicc <ping.u@ya.ru>
2024-03-25 17:26:04 +0300

    Deleted TestExample.java

--- app/src/test/java/TestExample.java
+++
@@ -1,9 +0,0 @@
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-public class TestExample {
-    @Test
-    void test1() {
-        assertTrue(true);
-    }
-}

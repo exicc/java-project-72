af589e4590af9508c7b1db42f8b7cda5edf33952
exicc <ping.u@ya.ru>
2024-04-10 20:18:19 +0300

    Fixed models

--- app/src/main/java/hexlet/code/model/Url.java
+++ app/src/main/java/hexlet/code/model/Url.java
@@ -1,17 +1,25 @@
 package hexlet.code.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 
 @Setter
 @Getter
-public class Url {
+@AllArgsConstructor
+public final class Url {
     Long id;
     String name;
     Timestamp createdAt;
     public Url(String name, Timestamp createdAt) {
         this.name = name;
         this.createdAt = createdAt;
+    }
+
+    public String getFormattedTimestamp() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm");
+        return dateFormat.format(createdAt);
     }
 }
---
+++ app/src/main/java/hexlet/code/model/UrlCheck.java
@@ -0,0 +1,40 @@
+package hexlet.code.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+
+@Getter
+@Setter
+public final class UrlCheck {
+
+    private long id;
+    private int statusCode;
+    private String title;
+    private String h1;
+    private String description;
+    private long urlId;
+    private Timestamp createdAt;
+
+    public UrlCheck(int statusCode, String title, String h1, String description, long urlId) {
+        this.statusCode = statusCode;
+        this.title = title;
+        this.h1 = h1;
+        this.description = description;
+        this.urlId = urlId;
+    }
+    public UrlCheck(int statusCode, String title, String h1, String description, long urlId, Timestamp createdAt) {
+        this.statusCode = statusCode;
+        this.title = title;
+        this.h1 = h1;
+        this.description = description;
+        this.urlId = urlId;
+        this.createdAt = createdAt;
+    }
+    public String getFormattedTimestamp() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm");
+        return dateFormat.format(createdAt);
+    }
+}

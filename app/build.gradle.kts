f8ec6b1be9441b72ca812ac68c6d9c07c70480b9
exicc <ping.u@ya.ru>
2024-04-10 20:17:30 +0300

    Added unirest and mockwebserver dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,10 +31,14 @@ dependencies {
     implementation("io.javalin:javalin:6.1.3")
     implementation("io.javalin:javalin-bundle:6.1.3")
     implementation("io.javalin:javalin-rendering:6.1.3")
+    implementation("com.konghq:unirest-java:4.0.0-RC1")
+    implementation("com.konghq:unirest-modules-gson:4.3.0")
+    implementation("org.jsoup:jsoup:1.17.2")
 
     testImplementation("org.assertj:assertj-core:3.24.2")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
 }
 
 tasks.test {

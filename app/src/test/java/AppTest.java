846f392fcc8402f16edb1c5aa8ecee748962e0e4
exicc <ping.u@ya.ru>
2024-04-13 02:29:26 +0300

    Added actual tests

--- app/src/test/java/AppTest.java
+++ app/src/test/java/AppTest.java
@@ -1,21 +1,24 @@
 import hexlet.code.App;
 import hexlet.code.model.Url;
+import hexlet.code.model.UrlCheck;
+import hexlet.code.repository.UrlCheckRepository;
 import hexlet.code.repository.UrlRepository;
+import io.javalin.Javalin;
+import io.javalin.testtools.JavalinTest;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.fail;
 
 import java.io.IOException;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.util.Optional;
 
-import io.javalin.Javalin;
-import io.javalin.testtools.JavalinTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTest {
 
@@ -23,11 +26,14 @@ public class AppTest {
     static MockWebServer server;
     static MockResponse response;
     static String domain;
+    static Optional<Url> optionalUrl;
+    static UrlCheck urlCheck;
+
     static String fakePageContent = "<html><head><title>Fake Page</title>"
             + "<meta name=\"description\" content=\"This is a fake page for testing purposes\"></head>"
             + "<body><h1>Welcome to Fake Page</h1><p>This is a fake page for testing purposes</p></body></html>";
     @BeforeAll
-    public static void beforeAll() throws IOException {
+    public static void beforeAll() throws IOException, SQLException {
         server = new MockWebServer();
         response = new MockResponse().setResponseCode(200).setBody(fakePageContent);
         server.enqueue(response);
@@ -39,76 +45,83 @@ public class AppTest {
         server.shutdown();
     }
     @BeforeEach
-    public final void setup() throws IOException, SQLException {
+    public final void setup() throws SQLException, IOException {
         app = App.getApp();
-    }
-    @Test
-    public void testMainPage() {
-        JavalinTest.test(app, (server, client) -> {
-            var response = client.get("/");
-            assertThat(response.code()).isEqualTo(200);
-            assertThat(response.body().string()).contains("Анализатор страниц");
-        });
-    }
-    @Test
-    public void testUrlsPage() {
-        JavalinTest.test(app, (server, client) -> {
-            var response = client.get("/urls");
-            assertThat(response.code()).isEqualTo(200);
-            assertThat(response.body().string()).contains("Сайты");
-        });
-    }
-    @Test
-    public void testUrlPage() throws SQLException {
-        var url = new Url(domain, new Timestamp(System.currentTimeMillis()));
-        UrlRepository.save(url);
-        JavalinTest.test(app, (server, client) -> {
-            var response = client.get("/urls/" + url.getId());
-            assertThat(response.code()).isEqualTo(200);
-            assertThat(response.body().string()).contains(domain);
-        });
-    }
-    @Test
-    void testUrlNotFound()  {
-        JavalinTest.test(app, (server, client) -> {
-            var response = client.get("/urls/999999");
-            assertThat(response.code()).isEqualTo(404);
-        });
-    }
-    @Test
-    public void testCreateUrl() {
-        Url url = new Url(domain, new Timestamp(System.currentTimeMillis()));
-        JavalinTest.test(app, (server, client) -> {
-            var requestBody = "url=" + domain;
-            var response = client.post("/urls", requestBody);
-            try {
-                UrlRepository.save(url);
+        var newUrl = new Url(domain, new Timestamp(System.currentTimeMillis()));
+        UrlRepository.save(newUrl);
+        optionalUrl = UrlRepository.getUrlByName(domain);
 
-                var targetUrl = UrlRepository.findByDomain(domain);
-                assertThat(targetUrl)
-                        .as("Check if URL with domain %s is present in the database", domain)
-                        .isPresent();
+        var newUrlCheck = new UrlCheck(200,
+                "Fake Page",
+                "Fake Page",
+                "fake page",
+                optionalUrl.get().getId());
+        UrlCheckRepository.save(newUrlCheck);
+        urlCheck = UrlCheckRepository.getAllUrlChecks().getLast();
+    }
+    @Nested
+    class UrlTest {
+        @Test
+        public void testMainPage() {
+            JavalinTest.test(app, (server, client) -> {
+                var response = client.get("/");
+                assertThat(response.code()).isEqualTo(200);
+                assertThat(response.body().string()).contains("Анализатор страниц");
+            });
+        }
 
-                assertThat(targetUrl.get().getName())
-                        .as("Check if the name of the retrieved URL contains the domain")
-                        .contains(domain);
-
-                assertThat(response.code())
-                        .isEqualTo(200);
-
+        @Test
+        public void testUrlsPage() {
+            JavalinTest.test(app, (server, client) -> {
+                var response = client.get("/urls");
+                assertThat(response.code()).isEqualTo(200);
                 assertThat(response.body().string())
-                        .as("Check if the response body contains the expected URL")
-                        .contains(domain);
+                        .contains(optionalUrl.get().getName())
+                        .contains(optionalUrl.get().getId().toString());
+            });
+        }
 
-                var urlId = targetUrl.get().getId();
+        @Test
+        public void testUrlPage() {
 
-                var checkResponse = client.post("/urls/" + urlId + "/checks");
-                assertThat(checkResponse.code())
-                        .as("Check if the response code for checking URL is correct")
+            JavalinTest.test(app, (server, client) -> {
+                var response = client.get("/urls/" + optionalUrl.get().getId().toString());
+                assertThat(response.code()).isEqualTo(200);
+                assertThat(response.body().string())
+                        .contains(optionalUrl.get().getName())
+                        .contains(String.valueOf(urlCheck.getStatusCode()));
+            });
+        }
+
+        @Test
+        void testUrlNotFound() {
+            JavalinTest.test(app, (server, client) -> {
+                var response = client.get("/urls/999999");
+                assertThat(response.code()).isEqualTo(404);
+            });
+        }
+    }
+
+    @Nested
+    class UrlCheckTest {
+        @Test
+        void test() {
+            JavalinTest.test(app, (server, client) -> {
+                var requestBody = "url=" + domain;
+                assertThat(client.post("/urls", requestBody).code()).isEqualTo(200);
+
+                assertThat(optionalUrl.isPresent()).isTrue();
+                assertThat(optionalUrl.get().getName()).isEqualTo(domain);
+
+                client.post("/urls/" + optionalUrl.get().getId() + "/checks");
+
+                assertThat(client.get("/urls/" + optionalUrl.get().getId()).code())
                         .isEqualTo(200);
-            } catch (Exception ex) {
-                fail("An unexpected exception occurred: " + ex.getMessage());
-            }
-        });
+                assertThat(urlCheck).isNotNull();
+                assertThat(urlCheck.getTitle()).contains("Fake Page");
+                assertThat(urlCheck.getH1()).contains("Fake Page");
+                assertThat(urlCheck.getDescription()).contains("fake page");
+            });
+        }
     }
 }
